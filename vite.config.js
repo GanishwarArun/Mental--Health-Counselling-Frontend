9e67b097fb7bf1576f42ba34ff200a354cb237b7
jagan guvi <akjagankumar@gmail.com>
2024-12-16 02:02:14 +0530

    final pro guvi fe

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './', // Ensures correct relative path for assets in production
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Sets the output directory for production build
+  },
   server: {
     mimeTypes: {
       '.js': 'application/javascript',
@@ -11,3 +14,16 @@ export default defineConfig({
     },
   },
 });
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     mimeTypes: {
+//       '.js': 'application/javascript',
+//       '.css': 'text/css',
+//     },
+//   },
+// });
